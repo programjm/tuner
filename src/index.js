a688917d9e3d310e372a859c624edfa88f0c130b
Jonathan Moore <programjm@gmail.com>
2019-12-08 18:59:56 -0600

    Prevent screen from going to sleep

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
+import NoSleep from 'nosleep.js'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// Prevent screen from going to sleep
+const noSleep = new NoSleep();
+document.addEventListener('click', function enableNoSleep() {
+    document.removeEventListener('click', enableNoSleep, false);
+    noSleep.enable();
+}, false);
